6e026c94984075419bff806aedc123a8b6ddd107
KSchlendrian <cbii@hotmail.de>
2023-08-21 15:15:23 +0200

    ListItem added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.scss'
-import Home from './components/pages/Home/Home'
-import AllGames from './components/pages/AllGames/AllGames'
-import RecentlyAdded from './components/pages/RecentlyAdded/RecentlyAdded'
-import Details from './components/pages/Details/Details'
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Home from "./components/pages/Home/Home";
+import AllGames from "./components/pages/AllGames/AllGames";
+import RecentlyAdded from "./components/pages/RecentlyAdded/RecentlyAdded";
+import Details from "./components/pages/Details/Details";
+import ListItem from "./components/shared/ListItem/ListItem";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/allgames' element={<AllGames/>}/>
-      <Route path='/recentlyadded' element={<RecentlyAdded/>}/>
-      <Route path='/game/:id' element={<Details/>}/>
-    </Routes>
-
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/allgames"
+          element={<AllGames />}
+        />
+        <Route
+          path="/recentlyadded"
+          element={<RecentlyAdded />}
+        />
+        <Route
+          path="/game/:id"
+          element={<Details />}
+        />
+        <Route
+          path="/listitem"
+          element={<ListItem />}
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/shared/ListItem/ListItem.jsx
+++ src/components/shared/ListItem/ListItem.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import { data } from "../../../assets/utils/data/data";
+import Button from "../Button/Button";
+
 const ListItem = () => {
-  return ( 
-    <>
-    </>
+  const [gameItems, setGameItems] = useState();
+
+  return (
+    <section>
+      <img
+        src={data[0].thumbnail}
+        alt=""
+      />
+      <div>{data[0].title}</div>
+      <Button
+        title={"Read More"}
+        path={`game/${data[0].id}`}
+      />
+      <a>{data[0].platform}</a>
+      <a>{data[0].genre}</a>
+      {/* publisher muss durch den "sci-fi"-key ersetzt werden! weiss aktuell nicht den key der "sci-fi" ausgibt!*/}
+      {/* <a>{data[0].publisher}</a> */}
+    </section>
   );
-}
+};
 
 export default ListItem;
