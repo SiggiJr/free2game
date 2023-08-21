1898d24e0a6413735c7a3357113ad0063516b9f1
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-08-21 15:58:26 +0200

    AllGames + RecentyAdded components, Grid module

--- src/components/pages/RecentlyAdded/RecentlyAdded.jsx
+++ src/components/pages/RecentlyAdded/RecentlyAdded.jsx
@@ -1,7 +1,36 @@
+import { data } from '../../../assets/utils/data/data.js'
+
+import ListItem from '../../shared/ListItem/ListItem.jsx'
+import Button from '../../shared/Button/Button.jsx'
+
+import styles from './RecentlyAdded.module.scss'
+import gridStyle from '../../../modules/Grid.module.scss'
+
 const RecentlyAdded = () => {
-  return ( 
-  <>
-  </>
+
+  console.log(data);
+
+  return (
+    <section className={styles["recently-added"]}>
+      <header className={styles.hero}>
+        <h1>Recently Added</h1>
+      </header>
+      <Button title={"Back to Home"} path={`/`} />
+      <div className={gridStyle["list-wrapper"]}>
+        {data.map((game) => {
+          return (
+            <ListItem
+              key={game.id}
+              data={game}
+            />
+            //   <article>
+            //   <h3>{game.title}</h3>
+            //   <p>{game.genre}</p>
+            // </article>
+          )
+        })}
+      </div>
+    </section>
   );
 }
 
