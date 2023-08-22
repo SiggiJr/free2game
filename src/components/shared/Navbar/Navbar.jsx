0d30fe1a9921d5fdddac550e69b21666960415b6
KSchlendrian <cbii@hotmail.de>
2023-08-22 16:57:36 +0200

    Navbar started

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,45 +1,78 @@
 import style from "./Navbar.module.scss";
 import BurgerIcon from "../../../assets/img/BurgerIcon.svg";
+import CloseIcon from "../../../assets/img/CloseIcon.png";
 import HomeIcon from "../../../assets/img/HomeIcon.svg";
 import AllGamesIcon from "../../../assets/img/AllGamesIcon.svg";
 import RecentlyAddedIcon from "../../../assets/img/RecentlyAddedIcon.svg";
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const [homeIconActive, setHomeIconActive] = useState(false);
+  const [navWidth, setNavWidth] = useState("5vw");
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const handleHomeIconClick = () => {
-    setHomeIconActive(!homeIconActive);
+  const toggleNav = () => {
+    if (isNavOpen) {
+      closeNav();
+    } else {
+      openNav();
+    }
+  };
+
+  const openNav = () => {
+    setNavWidth("250px");
+    setIsNavOpen(true);
+  };
+
+  const closeNav = () => {
+    setNavWidth("5vw");
+    setIsNavOpen(false);
   };
 
   return (
     <>
-      <nav className={style.nav}>
+      <nav
+        className={style.nav}
+        style={{ width: navWidth }}
+        id="nav">
         <div>
-          <img
-            src={BurgerIcon}
-            alt=""
-          />
+          <div onClick={toggleNav}>
+            <img
+              className={style.nav_toggle}
+              src={isNavOpen ? CloseIcon : BurgerIcon}
+              alt=""
+            />
+          </div>
         </div>
         <div className={style.nav_icons}>
           <div>
-            <img
-              className={`${style.nav_icons_home} ${
-                homeIconActive ? style.active : ""
-              }`}
-              src={HomeIcon}
-              alt=""
-            />
-            <img
-              className={style.nav_icons_all_games}
-              src={AllGamesIcon}
-              alt=""
-            />
-            <img
-              className={style.nav_icons_recently_added}
-              src={RecentlyAddedIcon}
-              alt=""
-            />
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => (isActive ? `${style.active}` : "")}>
+              <img
+                className={style.nav_icons_home}
+                src={HomeIcon}
+                alt=""
+              />
+            </NavLink>
+            <NavLink
+              to={"/allgames"}
+              className={({ isActive }) => (isActive ? `${style.active}` : "")}>
+              <img
+                className={style.nav_icons_all_games}
+                src={AllGamesIcon}
+                alt=""
+              />
+            </NavLink>
+            <NavLink
+              to={"/recentlyadded"}
+              className={({ isActive }) => (isActive ? `${style.active}` : "")}>
+              <img
+                className={style.nav_icons_recently_added}
+                src={RecentlyAddedIcon}
+                alt=""
+              />
+            </NavLink>
           </div>
         </div>
       </nav>
