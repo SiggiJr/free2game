cd1d0bfbef319b367ad12c0f8929dde843d4a5da
SiggiJr <fsiegert1@gmail.com>
2023-08-23 10:24:52 +0200

    API Key Einbindung Netlify

--- src/assets/utils/api/api.js
+++ src/assets/utils/api/api.js
@@ -12,6 +12,8 @@ const options = {
 };
 
 export const getGamesByFilter = (filter) => {
+  console.log(apiKey);
+  console.log(apiHost);
   return fetch(`${apiLink}?${filter}`, options)
     .then((response) => {
       if (!response.ok) throw new Error(`Fetch failed: ${response.statusText}`);
